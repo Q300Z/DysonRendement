390c22df72fe60132ce213c86c9edd8e241c6d92
YaroslavLeGrand <yaroslav.follet@campus-la-chataigneraie.org>
2023-11-28 14:20:39 +0100

    dernier commit

---
+++ DysonRendement/Platforms/Windows/MainActivity.cs
@@ -0,0 +1,10 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace DysonRendement;
+
+[Activity(Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
+{
+}
